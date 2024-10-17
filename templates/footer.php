d2e80d8e06300dfcf7f362fad9ab0ad63475837e
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-10-18 04:51:45 +0600

    Implement Footer

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+
+
+<footer class="section">
+    <div class="center grey-text">
+        @Copyright, 2024 Pizza Cut. <a href="https://shahinuralambhuiyan.vercel.app/" target="_blank">Bhuiyan</a>
+    </div>
+</footer>
+
+</body>
